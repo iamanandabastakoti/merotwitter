e7fb7b2dfe3c26968151b67a3687d671e4f253eb
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-05-24 12:09:30 +0545

    designed to post the tweet using axios.post

--- src/components/home/UserTweet.jsx
+++ src/components/home/UserTweet.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const UserTweet = () => {
+    let [tweetContent, setTweetContent] = useState("");
+
+    const upload = async () => {
+        try{
+            await axios.post("https://react-workshop-todo.fly.dev/posts", {
+                content: tweetContent,
+                image: "https://media.kulfyapp.com/0UY5UD/0UY5UD-360.gif",
+            }, {
+                headers: {
+                    apiKey: `${import.meta.env.VITE_API_KEY}`
+                }
+            })
+        } catch{
+            alert("Error Uploading Tweet!");
+        }
+    }
+
+    const handleSubmit = () => {
+        upload();
+    }
+
     return (
         <div className="user-tweet">
             <div className="tweetArea">
@@ -8,11 +30,11 @@ const UserTweet = () => {
                     <img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg" alt="User Profile Picture" />
                 </div>
                 <div className="write-tweet-text">
-                <textarea className='tweet-textarea' id="tweet" name="tweet" placeholder="What is happening?!" ></textarea>
+                <textarea className='tweet-textarea' id="tweet" name="tweet" placeholder="What is happening?!" required value={tweetContent} onChange={(e) => setTweetContent(e.target.value)} ></textarea>
                 </div>
             </div>
             <div className="home-tweet-button">
-            <button className="tweetButton">Tweet</button>
+            <button className="tweetButton" onClick={handleSubmit}>Tweet</button>
             </div>
         </div>
     )
