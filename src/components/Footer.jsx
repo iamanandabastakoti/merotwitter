983e889cdefe860db38b5f845f5bf32afd87a4f5
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-05-24 11:14:49 +0545

    designing the tweet-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {FaRegComment, FaRetweet, FaRegHeart, FaRegChartBar} from 'react-icons/fa'
+import {BsUpload} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="footer-icons">
+        <FaRegComment />
+      </div>
+      <div className="footer-icons">
+        <FaRetweet />
+      </div>
+      <div className="footer-icons">
+        <FaRegHeart />
+      </div>
+      <div className="footer-icons">
+        <FaRegChartBar />
+      </div>
+      <div className="footer-icons">
+        <BsUpload />
+      </div>
+    </div>
   )
 }
 
