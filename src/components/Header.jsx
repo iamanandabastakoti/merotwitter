e56c9ed9af8d967eebb8aa6e66745c8f5031b721
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-05-24 10:13:15 +0545

    added more option in tweet-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { FiMoreHorizontal } from 'react-icons/fi'
 
 const Header = () => {
   return (
     <div className="tweet-header">
-            <h3 className="user-name">Ananda Bastakoti</h3>
-            <span className="user-handle">@iamanandabastaktoi</span>
-        </div>
+      <div className="header-user-info">
+      <h3 className="user-name">Ananda Bastakoti</h3>
+      <span className="user-handle">@iamanandabastaktoi</span>
+      </div>
+      <div className="header-more">
+        <FiMoreHorizontal />
+      </div>
+    </div>
   )
 }
 
