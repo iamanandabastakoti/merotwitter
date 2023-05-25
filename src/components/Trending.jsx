0045991f87d5c3ee724d63da11a81d9ab1d2f2c6
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-05-25 23:01:09 +0545

    passing trending-details via object to trending.jsx

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {FiMoreHorizontal} from 'react-icons/fi'
 
-const Trending = () => {
+const Trending = ({ trendTitle, trendingContent, totalTweets }) => {
   return (
     <>
     <div className="trending-card">
         <div className="trend-details">
-            <div className="trending-title">Entertainment - Trending</div>
-            <div className="trending-content">Shah Rukh Khan</div>
-            <div className="total-trend-tweets">200k Tweets</div>
+            <div className="trending-title">{trendTitle}</div>
+            <div className="trending-content">{trendingContent}</div>
+            <div className="total-trend-tweets">{totalTweets}</div>
         </div>
         <div className="trend-more-option">
             <FiMoreHorizontal />
--- src/components/Trends.jsx
+++ src/components/Trends.jsx
@@ -3,6 +3,31 @@ import {BiSearch} from 'react-icons/bi'
 import Trending from './Trending'
 
 const Trends = () => {
+
+  const trends = [
+    {
+      trendTitle: 'Entertainment - Trending',
+      trendingContent: 'Shah Rukh Khan',
+      totalTweets: '200K Tweets'
+    },
+    {
+      trendTitle: 'Entertainment - Trending',
+      trendingContent: 'JAWAN',
+      totalTweets: '184K Tweets'
+    },
+    {
+      trendTitle: 'Entertainment - Trending',
+      trendingContent: '#SRK🐐',
+      totalTweets: '190K Tweets'
+    },
+    {
+      trendTitle: 'Sports - Trending',
+      trendingContent: '#ViratKohli🐐',
+      totalTweets: '100K Tweets'
+    },
+
+  ]
+
   return (
     <div className="trends">
       <div className="trends-left">
@@ -16,8 +41,11 @@ const Trends = () => {
             <h3>Trends for you</h3>
           </div>
           <div className="trending">
-            <Trending />
-            <Trending />
+            {trends.map(({trendTitle, trendingContent, totalTweets}) => {
+              return(
+                <Trending trendTitle={trendTitle} trendingContent={trendingContent} totalTweets={totalTweets} />
+              )
+            })}
           </div>
           <div className="show-more">
             Show More
