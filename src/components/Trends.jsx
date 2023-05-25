73ed12f9715dd9f98cd20c134468db27691be3df
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-05-25 16:45:49 +0545

    designed trends-section search bar

--- src/components/Trends.jsx
+++ src/components/Trends.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {BiSearch} from 'react-icons/bi'
 
 const Trends = () => {
   return (
     <div className="trends">
-        Trends
+      <div className="trends-left">
+        <div className="search-bar">
+          <BiSearch className='trends-search-icon' />
+          <input type="text" placeholder='Search Twitter' />
+        </div>
+      </div>
+      <div className="trends-right"></div>
     </div>
   )
 }
