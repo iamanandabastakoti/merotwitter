a9c02b271c740e24959c2f1e893729edca0cbda8
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-05-26 17:20:16 +0545

    singletweet showa tweet which is clicked using api

--- src/pages/SingleTweet.jsx
+++ src/pages/SingleTweet.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import Tweet from '../components/home/Tweet';
+import axios from 'axios';
 
 const SingleTweet = () => {
+
+  const [tweet, setTweet] = useState(null);
+  const{id} = useParams();
+  const fetchTweets = async () => {
+    const resp = await axios.get("https://react-workshop-todo.fly.dev/posts/" + id, {
+      headers: {
+        apiKey: `${import.meta.env.VITE_API_KEY}`
+      }
+    });
+    setTweet(resp.data.post);
+  }
+  useEffect(() => {
+    fetchTweets();
+  }, [])
+
   return (
-    <div>SingleTweet</div>
+    <>
+    {
+      tweet ? <Tweet avatar={'https://avatars.githubusercontent.com/u/' + tweet.user.githubId + '?v=4'} name={tweet.user.fullname} username={'@' + tweet.user.name} tweet={tweet.content} image={tweet.image} key={tweet._id} /> : null
+    }
+    </>
   )
 }
 
