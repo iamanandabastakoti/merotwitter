ac5acf62172b9d7a5069d51426fc589dd9e3f667
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-05-27 13:08:06 +0545

    addded loading-triangle until tweets are fetched

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,25 @@ import HomeTitle from '../components/home/HomeTitle'
 import UserTweet from '../components/home/UserTweet'
 import Tweet from '../components/home/Tweet'
 import axios from 'axios'
+import { Triangle } from 'react-loader-spinner'
 
 const Home = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const [users, setUsers] = useState([]);
   const [shouldRefresh, setShouldRefresh] = useState(false);
   const fetchTweets = async () => {
-    const posts = await axios.get("https://react-workshop-todo.fly.dev/posts/all?limit=100",{
+    const posts = await axios.get("https://react-workshop-todo.fly.dev/posts/all?limit=100", {
       headers: {
         apiKey: `${import.meta.env.VITE_API_KEY}`
       }
     });
-    setUsers(posts.data)
+    setUsers(posts.data);
+    setIsLoading(false);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchTweets();
   }, [shouldRefresh]);
 
@@ -25,7 +29,21 @@ const Home = () => {
     <div className="home">
       <HomeTitle />
       <UserTweet setShouldRefresh={setShouldRefresh} />
-      {users.map(({user, content, image, _id}) => {
+      {isLoading ? 
+      <div className="loading-triangle">
+        <Triangle
+          height="80"
+          width="80"
+          color="#1d9bf0"
+          ariaLabel="triangle-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+          /> 
+      </div>
+      : null
+      }
+      {users.map(({ user, content, image, _id }) => {
         return <Tweet avatar={'https://avatars.githubusercontent.com/u/' + user.githubId + '?v=4'} name={user.fullname} username={'@' + user.name} tweet={content} image={image} key={user.id} id={_id} />
       })}
     </div>
