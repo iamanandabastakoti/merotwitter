3ff38327185cd2a5a0bee24d3dec91cb7828cfd8
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-05-27 13:27:20 +0545

    added laoding animation in single-tweet

--- src/pages/SingleTweet.jsx
+++ src/pages/SingleTweet.jsx
@@ -3,8 +3,11 @@ import { useParams } from 'react-router-dom';
 import Tweet from '../components/home/Tweet';
 import axios from 'axios';
 import CommentBox from '../components/CommentBox';
+import { Triangle } from 'react-loader-spinner'
 
 const SingleTweet = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const [tweet, setTweet] = useState(null);
   const{id} = useParams();
@@ -15,6 +18,7 @@ const SingleTweet = () => {
       }
     });
     setTweet(resp.data.post);
+    setIsLoading(false);
   }
   useEffect(() => {
     fetchTweets();
@@ -22,6 +26,20 @@ const SingleTweet = () => {
 
   return (
     <>
+    {isLoading ? 
+      <div className="loading-triangle-singletweet">
+        <Triangle
+          height="80"
+          width="80"
+          color="#1d9bf0"
+          ariaLabel="triangle-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+          /> 
+      </div>
+      : null
+      }
     {
       tweet ? <Tweet avatar={'https://avatars.githubusercontent.com/u/' + tweet.user.githubId + '?v=4'} name={tweet.user.fullname} username={'@' + tweet.user.name} tweet={tweet.content} image={tweet.image} key={tweet._id} /> : null
     }
