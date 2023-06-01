f734b5c620dba5960088f851b88da91c71f5cee6
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-06-01 22:22:49 +0545

    ddesigned message section

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -6,6 +6,17 @@ const Messages = () => {
       <div className="messages-title">
         <h3>Messages</h3>
       </div>
+      <div className="messages-content">
+        <div className="messages-welcome">
+          <h1>Welcome to your inbox!</h1>
+        </div>
+        <div className="messages-description-content">
+          Drop a line, share Tweets and more with private conversations between you and others on Twitter.
+        </div>
+        <div className="arite-message-button">
+          <button className='message-button'>Write a message</button>
+        </div>
+      </div>
     </div>
   )
 }
