430fb55575785e42fd4617f53afa51eb6b1dd742
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-06-02 22:38:38 +0545

    using Link to navigate through pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,33 +7,9 @@ import { RiTodoLine } from 'react-icons/ri'
 import { TiSocialTwitterCircular } from 'react-icons/ti'
 import { CiCircleMore } from 'react-icons/ci'
 import { useNavigate } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-
-    const navigate = useNavigate();
-
-    const navigateToHome = () => {
-        navigate(`/`)
-    }
-
-    const navigateToExplore = () => {
-        navigate(`/explore`)
-    }
-    const navigateToNotifications = () => {
-        navigate(`/notifications`)
-    }
-    const navigateToMessages = () => {
-        navigate(`/messages`)
-    }
-    const navigateToLists = () => {
-        navigate(`/lists`)
-    }
-    const navigateToBookmarks = () => {
-        navigate(`/bookmarks`)
-    }
-    const navigateToTwitterBlue = () => {
-        navigate(`/twitterblue`)
-    }
 
     return (
         <div className="sidebar">
@@ -43,42 +19,60 @@ const Sidebar = () => {
                     <div className="appIcon optionList">
                         <BsTwitter />
                     </div>
-                    <div onClick={navigateToHome} className="optionList">
-                        <div className="icon"><BiHomeCircle /></div>
-                        <div className="optionName">Home</div>
-                    </div>
-                    <div onClick={navigateToExplore} className="optionList">
-                        <div className="icon"><BiHash /></div>
-                        <div className="optionName">Explore</div>
-                    </div>
-                    <div onClick={navigateToNotifications} className="optionList">
-                        <div className="icon"><GrNotification /></div>
-                        <div className="optionName">Notifications</div>
-                    </div>
-                    <div onClick={navigateToMessages} className="optionList">
-                        <div className="icon"><FiMail /></div>
-                        <div className="optionName">Messages</div>
-                    </div>
-                    <div onClick={navigateToLists} className="optionList">
-                        <div className="icon"><RiTodoLine /></div>
-                        <div className="optionName">Lists</div>
-                    </div>
-                    <div onClick={navigateToBookmarks} className="optionList">
-                        <div className="icon"><BiBookmark /></div>
-                        <div className="optionName">Bookmarks</div>
-                    </div>
-                    <div onClick={navigateToTwitterBlue} className="optionList">
-                        <div className="icon"><TiSocialTwitterCircular /></div>
-                        <div className="optionName">Twitter Blue</div>
-                    </div>
+                    <Link to='/'>
+                        <div className="optionList">
+                            <div className="icon"><BiHomeCircle /></div>
+                            <div className="optionName">Home</div>
+                        </div>
+                    </Link>
+                    <Link to='/explore'>
+                        <div className="optionList">
+                            <div className="icon"><BiHash /></div>
+                            <div className="optionName">Explore</div>
+                        </div>
+                    </Link>
+                    <Link to='/notifications'>
+                        <div className="optionList">
+                            <div className="icon"><GrNotification /></div>
+                            <div className="optionName">Notifications</div>
+                        </div>
+                    </Link>
+                    <Link to='/messages'>
+                        <div className="optionList">
+                            <div className="icon"><FiMail /></div>
+                            <div className="optionName">Messages</div>
+                        </div>
+                    </Link>
+                    <Link to='/lists'>
+                        <div className="optionList">
+                            <div className="icon"><RiTodoLine /></div>
+                            <div className="optionName">Lists</div>
+                        </div>
+                    </Link>
+                    <Link to='/bookmarks'>
+                        <div className="optionList">
+                            <div className="icon"><BiBookmark /></div>
+                            <div className="optionName">Bookmarks</div>
+                        </div>
+                    </Link>
+                    <Link to='/twitter-blue'>
+                        <div className="optionList">
+                            <div className="icon"><TiSocialTwitterCircular /></div>
+                            <div className="optionName">Twitter Blue</div>
+                        </div>
+                    </Link>
+                    <Link>
                     <div className="optionList">
                         <div className="icon"><BiUser /></div>
                         <div className="optionName">Profile</div>
                     </div>
+                    </Link>
+                    <Link>
                     <div className="optionList">
                         <div className="icon"><CiCircleMore /></div>
                         <div className="optionName">More</div>
                     </div>
+                    </Link>
                     <div className="optionList-button">
                         <button className="tweet-button">Tweet</button>
                     </div>
@@ -90,12 +84,12 @@ const Sidebar = () => {
                                 alt="Profile Picture"
                             />
                             <div className="sidebar-usser-info">
-                            <div className="sidebar-user-name">
-                                Ananda Bastakoti
-                            </div>
-                            <div className="sidebar-username">
-                                @iamanandabastakoti
-                            </div>
+                                <div className="sidebar-user-name">
+                                    Ananda Bastakoti
+                                </div>
+                                <div className="sidebar-username">
+                                    @iamanandabastakoti
+                                </div>
                             </div>
                         </div>
                     </div>
