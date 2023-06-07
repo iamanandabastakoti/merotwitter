7b30b5adfee436cf0521e94f313398a6cae084b7
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-06-07 08:37:20 +0545

    designing USerProfile section

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Tweet from '../components/home/Tweet'
 
 const UserProfile = () => {
 
@@ -7,7 +8,20 @@ const UserProfile = () => {
 
   return (
     <>
-    {console.log(userkey)}
+    <div className="user-profile">
+      <div className="user-profile-info">
+        <div className="user-profile-picture">
+          <img src="https://avatars.githubusercontent.com/u/105543272?v=4" alt="Profile Picture" />
+        </div>
+        <div className="user-name-info">
+          <h3>Ananda Bastakoti</h3>
+          <span>@iamanandabastakoti</span>
+        </div>
+      </div>
+      <div className="user-all-tweets">
+        <Tweet avatar='https://avatars.githubusercontent.com/u/105543272?v=4' name='Ananda Bastakoti' username='@iamanandabastakoti' tweet='My Profile' image='https://avatars.githubusercontent.com/u/105543272?v=4' />
+      </div>
+    </div>
     </>
   )
 }
