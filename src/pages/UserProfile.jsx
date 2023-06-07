f93eba6d544eb6405029928d323f1dd0c3d84faf
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-06-07 08:52:09 +0545

    added UserProfile title

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,27 +1,41 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import Tweet from '../components/home/Tweet'
+import { BiArrowBack } from "react-icons/bi";
 
 const UserProfile = () => {
 
-    const {userkey} = useParams();
+  const navigate = useNavigate();
+  const navigateToHome = () => {
+    navigate('/');
+  }
+
+  const { userkey } = useParams();
 
   return (
     <>
-    <div className="user-profile">
-      <div className="user-profile-info">
-        <div className="user-profile-picture">
-          <img src="https://avatars.githubusercontent.com/u/105543272?v=4" alt="Profile Picture" />
+      <div className="user-profile">
+        <div className="user-profile-header">
+          <div onClick={navigateToHome} className="user-profile-header-icon">
+            <BiArrowBack />
+          </div>
+          <div className="user-profile-title">
+            Ananda Bastakoti
+          </div>
         </div>
-        <div className="user-name-info">
-          <h3>Ananda Bastakoti</h3>
-          <span>@iamanandabastakoti</span>
+        <div className="user-profile-info">
+          <div className="user-profile-picture">
+            <img src="https://avatars.githubusercontent.com/u/105543272?v=4" alt="Profile Picture" />
+          </div>
+          <div className="user-name-info">
+            <h3>Ananda Bastakoti</h3>
+            <span>@iamanandabastakoti</span>
+          </div>
+        </div>
+        <div className="user-all-tweets">
+          <Tweet avatar='https://avatars.githubusercontent.com/u/105543272?v=4' name='Ananda Bastakoti' username='@iamanandabastakoti' tweet='My Profile' image='https://avatars.githubusercontent.com/u/105543272?v=4' />
         </div>
       </div>
-      <div className="user-all-tweets">
-        <Tweet avatar='https://avatars.githubusercontent.com/u/105543272?v=4' name='Ananda Bastakoti' username='@iamanandabastakoti' tweet='My Profile' image='https://avatars.githubusercontent.com/u/105543272?v=4' />
-      </div>
-    </div>
     </>
   )
 }
