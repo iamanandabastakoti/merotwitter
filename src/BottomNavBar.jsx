b56dcede2796a8b0a5db09da38ea9ba4aa9b650b
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2023-06-18 08:46:55 +0545

    BottomNavBar active option changes color

--- src/BottomNavBar.jsx
+++ src/BottomNavBar.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import { BiHomeCircle, BiSearch, GrNotification, FiMail } from "react-icons/all";
+import React, { useState } from 'react'
+import { BiHomeCircle, BiSearch, MdNotificationsNone, FiMail } from "react-icons/all";
 import { Link } from 'react-router-dom';
 
 const BottomNavBar = () => {
+
+    const [option, setOption] = useState(1);
+    const toggleOption = (index) => {
+        setOption(index);
+    }
+
     return (
         <div className="bottomNavBar">
             <div className="bottom-options">
                 <Link to='/'>
-                    <div className="bottom-optionList bottom-home">
+                    <div onClick={() => toggleOption(1)} className={ option === 1 ? "bottom-optionList bottom-home active-bottom-option" : "bottom-optionList bottom-home"}>
                         <BiHomeCircle />
                     </div>
                 </Link>
                 <Link to='/explore'>
-                    <div className="bottom-optionList bottom-search">
+                    <div onClick={() => toggleOption(2)} className={ option === 2 ? "bottom-optionList bottom-home active-bottom-option" : "bottom-optionList bottom-home"}>
                         <BiSearch />
                     </div>
                 </Link>
                 <Link to='/notifications'>
-                    <div className="bottom-optionList bottom-notification">
-                        <GrNotification />
+                    <div onClick={() => toggleOption(3)} className={ option === 3 ? "bottom-optionList bottom-home active-bottom-option" : "bottom-optionList bottom-home"}>
+                        <MdNotificationsNone />
                     </div>
                 </Link>
                 <Link to='/messages'>
-                    <div className="bottom-optionList bottom-message">
+                    <div onClick={() => toggleOption(4)} className={ option === 4 ? "bottom-optionList bottom-home active-bottom-option" : "bottom-optionList bottom-home"}>
                         <FiMail />
                     </div>
                 </Link>
